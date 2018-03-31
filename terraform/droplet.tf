4aff7ccf2e5fbc76358dafcbe94f4abde04ab80d
ykostiannikov <Yaroslav.Kostiannikov@lookout.com>
2018-03-31 14:04:01 +0300

    terraform update

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -2,25 +2,25 @@ resource "digitalocean_ssh_key" "deis-ssh-key" {
     name = "deis ssh key"
     public_key = "${file("${var.PUBLIC_SSH_KEY}")}"
 }
-resource "digitalocean_droplet" "deis-demo-1" {
+resource "digitalocean_droplet" "deis-project-1" {
     image = "coreos-beta"
-    name = "deis-demo-1"
+    name = "deis-project-1"
     region = "ams3"
     size = "1gb"
     private_networking = "true"
     ssh_keys = ["${digitalocean_ssh_key.deis-ssh-key.id}"]
 }
-resource "digitalocean_droplet" "deis-demo-2" {
+resource "digitalocean_droplet" "deis-project-2" {
     image = "coreos-beta"
-    name = "deis-demo-2"
+    name = "deis-project-2"
     region = "ams3"
     size = "1gb"
     private_networking = "true"
     ssh_keys = ["${digitalocean_ssh_key.deis-ssh-key.id}"]
 }
-resource "digitalocean_droplet" "deis-demo-3" {
+resource "digitalocean_droplet" "deis-project-3" {
     image = "coreos-beta"
-    name = "deis-demo-3"
+    name = "deis-project-3"
     region = "ams3"
     size = "1gb"
     private_networking = "true"
@@ -35,14 +35,14 @@ resource "digitalocean_droplet" "loadbalancer" {
     private_networking = "true"
     ssh_keys = ["${digitalocean_ssh_key.deis-ssh-key.id}"]
 }
-output "output-deis-demo-1" {
-  value = "- { host: ${digitalocean_droplet.deis-demo-1.ipv4_address}, fqdn: ${digitalocean_droplet.deis-demo-1.name}.example.com, ip: ${digitalocean_droplet.deis-demo-1.ipv4_address_private} }"
+output "output-deis-project-1" {
+  value = "- { host: ${digitalocean_droplet.deis-project-1.ipv4_address}, fqdn: ${digitalocean_droplet.deis-project-1.name}.example.com, ip: ${digitalocean_droplet.deis-project-1.ipv4_address_private} }"
 }
-output "output-deis-demo-2" {
-  value = "- { host: ${digitalocean_droplet.deis-demo-2.ipv4_address}, fqdn: ${digitalocean_droplet.deis-demo-2.name}.example.com, ip: ${digitalocean_droplet.deis-demo-2.ipv4_address_private} }"
+output "output-deis-project-2" {
+  value = "- { host: ${digitalocean_droplet.deis-project-2.ipv4_address}, fqdn: ${digitalocean_droplet.deis-project-2.name}.example.com, ip: ${digitalocean_droplet.deis-project-2.ipv4_address_private} }"
 }
-output "output-deis-demo-3" {
-  value = "- { host: ${digitalocean_droplet.deis-demo-3.ipv4_address}, fqdn: ${digitalocean_droplet.deis-demo-3.name}.example.com, ip: ${digitalocean_droplet.deis-demo-3.ipv4_address_private} }"
+output "output-deis-project-3" {
+  value = "- { host: ${digitalocean_droplet.deis-project-3.ipv4_address}, fqdn: ${digitalocean_droplet.deis-project-3.name}.example.com, ip: ${digitalocean_droplet.deis-project-3.ipv4_address_private} }"
 }
 
 output "output-deis-lb" {
